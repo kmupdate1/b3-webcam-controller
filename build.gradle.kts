561800154b935f2e5f27fb4186108193bd5a02be
Ken <lax.potcx1562@gmail.com>
2026-04-23 21:19:40 +0900

    依存関係解決成功？

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,33 @@ plugins {
     alias { libs.plugins.kotlin.multiplatform } apply false
 }
 
-group = "org.bluebikebase"
-version = ""
+val isRelease = project.hasProperty("release") && project.property("release") == "true"
+val v = rootProject.property("library.version")?.toString() ?: "unspecified"
+
+val repoType = if (isRelease) "releases" else "snapshots"
+val domain = rootProject.property("repository.domain")?.toString()
+val repositoryUri = "https://$domain/repository/bluebikebase-$repoType/"
+
+allprojects {
+    group = "org.bluebikebase"
+    version = if (isRelease) v else "$v-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri(repositoryUri)
+
+            credentials(PasswordCredentials::class) {
+                username = System.getenv("B3_REPO_USER")
+                password = System.getenv("B3_REPO_PASS")
+            }
+
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
