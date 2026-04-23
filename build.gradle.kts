9550d941784ea21ccea39e6b616555038485bf3c
Ken <lax.potcx1562@gmail.com>
2026-04-23 20:44:17 +0900

    b3-webcam-controllerの誕生

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("multiplatform") version "2.3.10"
+}
+
+group = "org.bluebikebase"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
