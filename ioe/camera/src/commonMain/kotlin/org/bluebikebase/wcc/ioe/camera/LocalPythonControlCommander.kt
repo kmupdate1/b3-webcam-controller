e273b940d848af8a21ed1760d22557cfdba7c282
Ken <lax.potcx1562@gmail.com>
2026-04-25 00:28:31 +0900

    Clientではなく、ローカルコマンドの送信者としてのCommander。

--- ioe/camera/src/commonMain/kotlin/org/bluebikebase/wcc/ioe/camera/LocalPythonControlCommander.kt
+++ ioe/camera/src/commonMain/kotlin/org/bluebikebase/wcc/ioe/camera/LocalPythonControlCommander.kt
@@ -1,12 +1,13 @@
 package org.bluebikebase.wcc.ioe.camera
 
+import kotlinx.coroutines.channels.Channel
 import org.bluebikebase.core.algebra.Vector
 import org.bluebikebase.core.geometry.ScalarDRange
 import org.bluebikebase.wcc.domain.camera.agreement.Motion
 import org.bluebikebase.wcc.domain.camera.agreement.Zoom
 import org.bluebikebase.wcc.domain.camera.entity.Camera
 
-class LocalPythonMotionClient : Motion, Zoom {
+class LocalPythonControlCommander : Motion, Zoom {
     override val horizontalLimit: ScalarDRange
         get() = TODO("Not yet implemented")
     override val verticalLimit: ScalarDRange
@@ -26,4 +27,7 @@ class LocalPythonMotionClient : Motion, Zoom {
     override fun zoom(target: Camera, velocity: Vector) {
         TODO("Not yet implemented")
     }
+
+    private val motionChannel = Channel<Pair<Vector, Vector>>(Channel.CONFLATED)
+    private val zoomChannel = Channel<Vector>(Channel.CONFLATED)
 }
--- ioe/camera/src/commonMain/kotlin/org/bluebikebase/wcc/ioe/camera/LocalRustControlCommander.kt
+++ ioe/camera/src/commonMain/kotlin/org/bluebikebase/wcc/ioe/camera/LocalRustControlCommander.kt
@@ -1,28 +1,13 @@
 package org.bluebikebase.wcc.ioe.camera
 
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.plugins.websocket.*
-import io.ktor.client.request.*
 import kotlinx.coroutines.channels.Channel
 import org.bluebikebase.core.algebra.Vector
 import org.bluebikebase.core.geometry.ScalarDRange
 import org.bluebikebase.wcc.domain.camera.agreement.Motion
 import org.bluebikebase.wcc.domain.camera.agreement.Zoom
 import org.bluebikebase.wcc.domain.camera.entity.Camera
-import org.bluebikebase.wcc.ioe.util.client
 
-class DirectWsMotionClient(
-    private val httpClient: HttpClient,
-) : Motion, Zoom {
-    suspend fun connect(target: Camera) {
-        val range = client.get("http://${target.ipv4Address}/config").body<ScalarDRange>()
-
-        client.webSocket(host = target.ipv4Address, path = "/control") {
-
-        }
-    }
-
+class LocalRustControlCommander : Motion, Zoom {
     override val horizontalLimit: ScalarDRange
         get() = TODO("Not yet implemented")
     override val verticalLimit: ScalarDRange
