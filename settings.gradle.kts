703287875e3b53cfbadd4df549b871f626970bd7
Ken <lax.potcx1562@gmail.com>
2026-04-24 10:57:50 +0900

    アーキテクチャ意識

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,9 @@
 rootProject.name = "b3-webcam-controller"
 
-include(":domain")
-include(":infrastructure")
-include(":application")
-include(":presentation")
+include(
+    ":core:domain",
+    ":core:application",
+    ":ioe:camera",
+    ":ioe:event",
+    ":kernel",
+)
