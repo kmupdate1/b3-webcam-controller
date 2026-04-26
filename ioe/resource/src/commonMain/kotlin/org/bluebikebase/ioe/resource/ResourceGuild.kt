fb2b51dde5725c9e5316ddfb78f15f17f6dc1470
Ken Murase <lax.potcx1562@gmail.com>
2026-04-27 05:48:28 +0900

    ある程度ベースできてきた。

--- ioe/resource/src/commonMain/kotlin/org/bluebikebase/ioe/resource/ResourceGuild.kt
+++ ioe/resource/src/commonMain/kotlin/org/bluebikebase/ioe/resource/ResourceGuild.kt
@@ -5,7 +5,12 @@ import kotlinx.coroutines.sync.withLock
 import org.bluebikebase.core.foundation.Identity
 import org.bluebikebase.ioe.resource.error.B3IoeResourceNotFoundException
 
-class ResourceGuild<T, R> private constructor() : Reception<T, R> {
+class ResourceGuild<T, R> private constructor(
+    private val withSingle: MutableMap<Identity, Boolean>,
+    private val recipes: MutableMap<Identity, suspend () -> T>,
+    private val disposes: MutableMap<Identity, suspend () -> Unit>,
+    private val managers: MutableMap<Identity, ResourceManager<T, R>>,
+) : Reception<T, R> {
     override suspend fun inviteTo(resourceId: Identity): ResourceManager<T, R> = managers[resourceId] ?: Mutex().run {
         withLock {
             val resource = recipes.getValue(resourceId).invoke()
@@ -24,37 +29,37 @@ class ResourceGuild<T, R> private constructor() : Reception<T, R> {
     internal suspend fun dispose(resourceId: Identity) = disposes[resourceId]?.invoke()
         ?: throw B3IoeResourceNotFoundException()
 
-    private val idOfTheFlyingDutchman = Identity.gen()
-    private val withSingle: MutableMap<Identity, Boolean> = mutableMapOf(idOfTheFlyingDutchman to true)
-    private val recipes: MutableMap<Identity, suspend () -> T> =
-        mutableMapOf(idOfTheFlyingDutchman to buildTheFlyingDutchman())
-    private val disposes: MutableMap<Identity, suspend () -> Unit> =
-        mutableMapOf(idOfTheFlyingDutchman to {})
-    private val managers: MutableMap<Identity, ResourceManager<T, R>> =
-        mutableMapOf(idOfTheFlyingDutchman to boardOnTheFlyingDutchman())
-
-    @Suppress("UNCHECKED_CAST")
-    private fun buildTheFlyingDutchman(): suspend () -> T = { null as T }
-    @Suppress("UNCHECKED_CAST")
-    private fun boardOnTheFlyingDutchman(): ResourceManager<T, R> = Ghost as ResourceManager<T, R>
-
     /**
      * ギルド（シングルトンを想定）を開店するための足掛かり
      */
-    class Builder<T, R> {
-        private val guild: ResourceGuild<T, R> = ResourceGuild()
+    class Builder<T, R> private constructor() {
+        private val guild: ResourceGuild<T, R> = ResourceGuild(
+            withSingle = mutableMapOf(),
+            recipes = mutableMapOf(),
+            disposes = mutableMapOf(),
+            managers = mutableMapOf(),
+        )
 
-        fun set(uuid: String, isSingle: Boolean = false, recipe: suspend () -> T, dispose: suspend () -> Unit): Builder<T, R> {
+        fun set(
+            strUuid: String, isSingle: Boolean = false,
+            recipe: suspend () -> T,
+            dispose: suspend () -> Unit,
+        ): Builder<T, R> {
             guild.apply {
-                val resourceId = Identity.fromString(uuid)
+                val resourceId = Identity.fromString(strUuid)
+
                 withSingle[resourceId] = isSingle
                 recipes[resourceId] = recipe
                 disposes[resourceId] = dispose
+                managers[resourceId] = boardOnTheFlyingDutchman()
             }
 
             return this
         }
 
         fun open(): ResourceGuild<T, R> = guild
+
+        @Suppress("UNCHECKED_CAST")
+        private fun boardOnTheFlyingDutchman(): ResourceManager<T, R> = Ghost as ResourceManager<T, R>
     }
 }
