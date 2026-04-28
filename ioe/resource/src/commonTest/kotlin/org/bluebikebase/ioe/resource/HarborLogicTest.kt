5a4de46e062fb4470d98a9ed23c6c62b4c1f5489
Ken <lax.potcx1562@gmail.com>
2026-04-28 20:46:32 +0900

    可視性強化

--- ioe/resource/src/commonTest/kotlin/org/bluebikebase/ioe/resource/HarborLogicTest.kt
+++ ioe/resource/src/commonTest/kotlin/org/bluebikebase/ioe/resource/HarborLogicTest.kt
@@ -1,5 +1,6 @@
 package org.bluebikebase.ioe.resource
 
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.currentCoroutineContext
@@ -7,11 +8,14 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 import org.bluebikebase.core.foundation.Identity
 import org.bluebikebase.core.foundation.ScalarL
 import org.bluebikebase.ioe.resource.authority.AuthorityBuilder
 import org.bluebikebase.ioe.resource.domain.VirtualSoySensor
+import kotlin.random.Random
 import kotlin.test.Test
+import kotlin.time.Duration.Companion.milliseconds
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -19,15 +23,15 @@ import kotlin.uuid.Uuid
 class HarborLogicTest {
     @Test
     fun `harbor logic test`() = runTest {
-        val shipA = withContext(currentCoroutineContext()) { authority.welcomeTo(Identity.fromString(uuid.toString())) }
-        val shipB = withContext(currentCoroutineContext()) { authority.welcomeTo(Identity.fromString(uuid.toString())) }
-        val shipC = withContext(currentCoroutineContext()) { authority.welcomeTo(Identity.fromString(uuid.toString())) }
-        val shipD = withContext(currentCoroutineContext()) { authority.welcomeTo(Identity.fromString(uuid.toString())) }
-        val shipE = withContext(currentCoroutineContext()) { authority.welcomeTo(Identity.fromString(uuid.toString())) }
+        val shipA = withContext(currentCoroutineContext()) { authority.welcomeTo(Identity.fromString(uuid1.toString())) }
+        val shipB = withContext(currentCoroutineContext()) { authority.welcomeTo(Identity.fromString(uuid1.toString())) }
+        val shipC = withContext(currentCoroutineContext()) { authority.welcomeTo(Identity.fromString(uuid1.toString())) }
+        val shipD = withContext(currentCoroutineContext()) { authority.welcomeTo(Identity.fromString(uuid1.toString())) }
+        val shipE = withContext(currentCoroutineContext()) { authority.welcomeTo(Identity.fromString(uuid1.toString())) }
 
         val transactionA = authority.dispatch {
             val value = shipA.operate { sensor ->
-                sensor.measure().also {
+                sensor.measure(100L).also {
                     println("Value: $it times")
                 }
             }
@@ -36,7 +40,7 @@ class HarborLogicTest {
         }
         val transactionB = authority.dispatch {
             val value = shipB.operate { sensor ->
-                sensor.measure().also {
+                sensor.measure(100L).also {
                     println("Value: $it times")
                 }
             }
@@ -45,7 +49,7 @@ class HarborLogicTest {
         }
         val transactionC = authority.dispatch {
             val value = shipC.operate { sensor ->
-                sensor.measure().also {
+                sensor.measure(100L).also {
                     println("Value: $it times")
                 }
             }
@@ -54,7 +58,7 @@ class HarborLogicTest {
         }
         val transactionD = authority.dispatch {
             val value = shipD.operate { sensor ->
-                sensor.measure().also {
+                sensor.measure(100L).also {
                     println("Value: $it times")
                 }
             }
@@ -63,7 +67,7 @@ class HarborLogicTest {
         }
         val transactionE = authority.dispatch {
             val value = shipE.operate { sensor ->
-                sensor.measure().also {
+                sensor.measure(100L).also {
                     println("Value: $it times")
                 }
             }
@@ -87,20 +91,48 @@ class HarborLogicTest {
         val jobs = (1..5).map { i ->
             async {
                 // 受付を通る（最初の一人だけが実体化をキックし、他は待機するはず）
-                val ship = authority.welcomeTo(Identity.fromString(uuid.toString()))
+                val shipA = authority.welcomeTo(Identity.fromString(uuid1.toString()))
+                val shipB = authority.welcomeTo(Identity.fromString(uuid2.toString()))
+                val shipC = authority.welcomeTo(Identity.fromString(uuid3.toString()))
 
                 // 出航命令を出す
-                authority.dispatch {
-                    val value = ship.operate { sensor ->
-                        println("[Job $i] センサー操作開始（ハッチ内）...")
+                authority.run {
+                    dispatch {
+                        val value = shipA.operate { sensor ->
+                            println("[Job $i] センサー操作開始（ハッチ内）...")
 
-                        println("[Job $i] 物理操作の時間をシミュレート (runTest 内なので delay は実際には 500ms 待たず、仮想時間を進める)")
-                        delay(5_000)
+                            println("[Job $i] 物理操作の時間をシミュレート (runTest 内なので delay は実際には 500ms 待たず、仮想時間を進める)")
+                            delay(5_000)
 
-                        sensor.measure().also { println("[Job $i] 計測完了: $it") }
+                            sensor.measure(3_000L).also { println("[Job $i] 計測完了: $it") }
+                        }
+
+                        Result.success(value)
                     }
+                    dispatch {
+                        val value = shipB.operate { sensor ->
+                            println("[Job $i] センサー操作開始（ハッチ内）...")
 
-                    Result.success(value)
+                            println("[Job $i] 物理操作の時間をシミュレート (runTest 内なので delay は実際には 500ms 待たず、仮想時間を進める)")
+                            delay(500)
+
+                            sensor.measure(100L).also { println("[Job $i] 計測完了: $it") }
+                        }
+
+                        Result.success(value)
+                    }
+                    dispatch {
+                        val value = shipC.operate { sensor ->
+                            println("[Job $i] センサー操作開始（ハッチ内）...")
+
+                            println("[Job $i] 物理操作の時間をシミュレート (runTest 内なので delay は実際には 500ms 待たず、仮想時間を進める)")
+                            delay(5_000)
+
+                            sensor.measure(30L).also { println("[Job $i] 計測完了: $it") }
+                        }
+
+                        Result.success(value)
+                    }
                 }
             }
         }
@@ -116,17 +148,69 @@ class HarborLogicTest {
 
         println("--- End: Concurrent Voyage Test ---")
     }
-    private val uuid = Uuid.random()
+
+    @Test
+    fun `random access logic test`() = runTest {
+        val jobs = (1..20).map { i ->
+            async {
+                // 1. バラバラのタイミングで現れるゲスト
+                delay(Random.nextLong(0, 1000))
+
+                try {
+                    // 2. せっかちなゲスト（2秒でリジェクト）
+                    withTimeout(2000.milliseconds) {
+                        val ship = authority.welcomeTo(Identity.fromString(uuid1.toString()))
+                        authority.dispatch {
+                            val value = ship.operate { sensor ->
+                                // 3. 物理的なゆらぎ
+                                delay(Random.nextLong(100, 3000))
+                                sensor.measure(30L)
+                            }
+
+                            Result.success(value)
+                        }
+                    }
+                } catch (e: TimeoutCancellationException) {
+                    println("[Job $i] あまりに長い！リジェクトして帰ります。")
+                    Result.failure(e)
+                }
+            }
+        }
+    }
+
+    private val uuid1 = Uuid.random()
+    private val uuid2 = Uuid.random()
+    private val uuid3 = Uuid.random()
+
     private val authority = AuthorityBuilder<VirtualSoySensor, ScalarL>()
         .register(
-            strUuid = uuid.toString(),
-            // isSingle = true,
+            strUuid = uuid1.toString(),
             establish = {
-                println("--- Sensor establishing... ---")
+                println("--- Sensor[1] establishing... ---")
                 VirtualSoySensor()
             },
-            cleanup = { println("--- Sensor terminating... ---") },
-            dispose = { println("---   Sensor power off    ---") },
+            cleanup = { println("--- Sensor[1] terminating... ---") },
+            dispose = { println("---   Sensor[1] powered off    ---") },
+        )
+        .register(
+            strUuid = uuid2.toString(),
+            isSingle = true,
+            establish = {
+                println("--- Sensor[2] establishing... ---")
+                VirtualSoySensor()
+            },
+            cleanup = { println("--- Sensor[2] terminating... ---") },
+            dispose = { println("--- Sensor[2] powered off ---") },
+        )
+        .register(
+            strUuid = uuid3.toString(),
+            isSingle = true,
+            establish = {
+                println("--- Sensor[3] establishing... ---")
+                VirtualSoySensor()
+            },
+            cleanup = { println("--- Sensor[3] terminating... ---") },
+            dispose = { println("--- Sensor[3] powered off    ---") },
         )
         .build()
 
--- ioe/resource/src/commonTest/kotlin/org/bluebikebase/ioe/resource/domain/VirtualSoySensor.kt
+++ ioe/resource/src/commonTest/kotlin/org/bluebikebase/ioe/resource/domain/VirtualSoySensor.kt
@@ -3,5 +3,5 @@ package org.bluebikebase.ioe.resource.domain
 import org.bluebikebase.core.foundation.ScalarL
 
 class VirtualSoySensor {
-    fun measure(): ScalarL = ScalarL.of(5_000L)
+    fun measure(expect: Long): ScalarL = ScalarL.of(expect)
 }
